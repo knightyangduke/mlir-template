0c7535c81b91e118b18ef47f7b9518765f46a33d
knightyangduke <knightyangduke@gmail.com>
2025-06-30 17:15:23 -0400

    dummy pass 2

--- lib/Transforms/StandaloneDummyPass.cpp
+++ lib/Transforms/StandaloneDummyPass.cpp
@@ -1,6 +1,7 @@
 //===- StandaloneDummyPattern.cpp --- A dummy pass
 //------------------------------*-===//
 
+#include "Dialect/StandaloneOps.h"
 #include "Transforms/Passes.h"
 #include "mlir/Dialect/Affine/Passes.h"
 
@@ -14,6 +15,7 @@
 #include "mlir/Dialect/Affine/Utils.h"
 #include "mlir/Dialect/Func/IR/FuncOps.h"
 #include "mlir/IR/Builders.h"
+#include "mlir/Transforms/GreedyPatternRewriteDriver.h"
 #include "llvm/Support/CommandLine.h"
 #include "llvm/Support/Debug.h"
 #include <optional>
@@ -24,6 +26,61 @@ using namespace mlir;
 
 namespace mlir {
 namespace {
+
+/// Pattern match for dummy CRL
+/// replace "foo" with "crl"
+
+/*
+struct FooToDummyCrlPattern : public mlir::OpRewritePattern<Operation> {
+  using OpRewritePattern::OpRewritePattern;
+
+  LogicalResult matchAndRewrite(Operation* op, PatternRewriter &rewriter) const override {
+    if(!canBeReplaced(op)) {
+      return failure();
+    }
+
+    // Create a new bar op with the same operands and result type
+    auto newOp = rewriter.create<standalone::CrlOp>(
+        op->getLoc(), op->getResults().getType(), op->getOperands());
+
+    // Replace the original foo op with the new bar op
+    rewriter.replaceOp(op, newOp->getResults());
+
+    return success();
+  }
+
+  bool canBeReplaced(Operation* op) const {
+    return (op->getDialect()->getNamespace() == "standalone");
+  }
+};
+*/
+
+struct FooToDummyCrlPattern : public RewritePattern  {
+
+  FooToDummyCrlPattern(PatternBenefit benefit, MLIRContext *context)
+      : RewritePattern(MatchAnyOpTypeTag(), benefit, context) {}
+  LogicalResult matchAndRewrite(Operation* op, PatternRewriter &rewriter) const override {
+    if(!canBeReplaced(op)) {
+      return failure();
+    }
+
+    // Create a new bar op with the same operands and result type
+    //auto newOp = rewriter.create<standalone::CrlOp>(
+    //    op->getLoc(), op->getResults().getType(), op->getOperands());
+
+    auto newOp = rewriter.create<standalone::UnaryOp>(
+        op->getLoc(), op->getResults().getType(), op->getOperands());
+
+    // Replace the original foo op with the new bar op
+    rewriter.replaceOp(op, newOp->getResults());
+
+    return success();
+  }
+
+  bool canBeReplaced(Operation* op) const {
+    return (op->getDialect()->getNamespace() == "standalone");
+  }
+};
 
 /// =========================================================
 /// Dummy Pattern
@@ -74,9 +131,17 @@ void StandaloneDummyCRLTransformPattern::runOnOperation() {
   // TODO: add your customised pattern implementation
   
   func::FuncOp func = getOperation(); // You get a function op directly
-
   llvm::outs() << "Running Dummy CRL replacement on function: " << func.getName() << "\n";
+
+  MLIRContext *ctx = &getContext();
+    RewritePatternSet patterns(ctx);
+    patterns.add<FooToDummyCrlPattern>(1,ctx);
+
+    if (failed(applyPatternsGreedily(func, std::move(patterns)))) {
+      signalPassFailure();
+    }
 }
+
 
 } // namespace
 
